e77abb5a893b782e39888e25d7821375d0da2aa2
Diego <diego.gliosca@gmail.com>
2018-01-28 18:56:00 +0000

    extract method into an object

--- src/test/kotlin/MoviesReader.kt
+++ src/test/kotlin/MoviesReader.kt
@@ -2,12 +2,21 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.junit.Test
 
-class MoviesReader {
+class MoviesReaderTest {
 
     @Test fun `read movies from file`() {
-        val movies = javaClass.classLoader.getResourceAsStream("movies.txt")
-        movies.use { val actual = it.reader().readLines()
-            assertThat(actual[0], equalTo("movie_title   year  award_winner  actors_comma_separated"))
+        val actual = MoviesReader.readMoviesFrom("movies.txt")
+
+        assertThat(actual[0], equalTo("movie_title   year  award_winner  actors_comma_separated"))
+    }
+
+    object MoviesReader {
+
+        fun readMoviesFrom(fileName: String): List<String> {
+            val movies = javaClass.classLoader.getResourceAsStream(fileName)
+            movies.use {
+                return it.reader().readLines()
+            }
         }
     }
 }
