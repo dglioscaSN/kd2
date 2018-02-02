08632919e087d5d597f580d2c628defce287292b
Diego <diego.gliosca@gmail.com>
2018-02-02 11:27:02 +0000

    movies with same actor name

--- src/main/kotlin/sn/VideoStore.kt
+++ src/main/kotlin/sn/VideoStore.kt
@@ -8,5 +8,13 @@ class VideoStore(val movies: List<Movie>) {
 
     fun awardWinner(awardWinner: Boolean, year: Int): List<Movie> = movies.filter { isAwardWinner(it) == awardWinner }.filter { it.year == year }
 
+    fun moviesWithSameActorName(actorName: String): List<Movie> {
+        return movies.filter { it.actors.containActor(actorName) }
+    }
+
     private fun isAwardWinner(it: Movie) = it.awardWinner == "yes"
+}
+
+private fun List<String>.containActor(actorName: String): Boolean {
+    return this.find { it.contains(actorName) } != null
 }
