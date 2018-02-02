6b58d21d9c7b6203b0a3394887be5e34a923c998
Diego <diego.gliosca@gmail.com>
2018-02-02 09:42:59 +0000

    find movies in a specific year

--- src/main/kotlin/sn/VideoStore.kt
+++ src/main/kotlin/sn/VideoStore.kt
@@ -1,7 +1,10 @@
 package sn
 
 class VideoStore(val movies: List<Movie>) {
-    fun findMovie(title: String): Movie? {
-        return movies.find { it.title == title }
+
+    fun findMovie(title: String): Movie? = movies.find { it.title == title }
+
+    fun cameOutIn(year: Int) : List<Movie> {
+        return movies.filter { it.year == year }
     }
 }
--- src/test/kotlin/sn/VideoStoreTest.kt
+++ src/test/kotlin/sn/VideoStoreTest.kt
@@ -13,4 +13,26 @@ class VideoStoreTest {
 
         assertThat(movie, equalTo(oceansEleven))
     }
+
+    @Test fun `find all movies came out in a specific year`() {
+        val oceansEleven = Movie("Ocean's Eleven", 2001, "no", listOf("George Clooney", "Brad Pitt", "Julia Roberts"))
+        val harryPotter = Movie("Harry Potter and the Sorcerer's Stone", 2001, "no", listOf("Daniel Radcliffe", " Rupert Grint", "Richard Harris"))
+        val donnieDarko = Movie("Donnie Darko", 2001, "no", listOf("Jake Gyllenhaal", "Jena Malone", "Mary McDonnell"))
+        val movieCollection = listOf(oceansEleven, harryPotter, donnieDarko)
+        val videoStore = VideoStore(movieCollection)
+        val movies = videoStore.cameOutIn(2001)
+
+        assertThat(movies, equalTo(movieCollection))
+    }
+
+    @Test fun `no movies found in a specific year`() {
+        val oceansEleven = Movie("Ocean's Eleven", 2001, "no", listOf("George Clooney", "Brad Pitt", "Julia Roberts"))
+        val harryPotter = Movie("Harry Potter and the Sorcerer's Stone", 2001, "no", listOf("Daniel Radcliffe", " Rupert Grint", "Richard Harris"))
+        val donnieDarko = Movie("Donnie Darko", 2001, "no", listOf( "Jake Gyllenhaal", "Jena Malone", "Mary McDonnell"))
+        val movieCollection = listOf(oceansEleven, harryPotter, donnieDarko)
+        val videoStore = VideoStore(movieCollection)
+        val movies = videoStore.cameOutIn(2002)
+
+        assertThat(movies, equalTo(emptyList()))
+    }
 }
