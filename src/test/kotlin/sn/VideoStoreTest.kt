4038b47cb147caf65be31eecf106ffc6f3736c32
Diego <diego.gliosca@gmail.com>
2018-02-02 11:39:44 +0000

    moved movies in a file

--- src/test/kotlin/sn/VideoStoreTest.kt
+++ src/test/kotlin/sn/VideoStoreTest.kt
@@ -7,55 +7,48 @@ import org.junit.Test
 class VideoStoreTest {
 
     @Test fun `find movie with a specified title`() {
-        val oceansEleven = Movie("Ocean's Eleven", 2001, "no", listOf("George Clooney", "Brad Pitt", "Julia Roberts"))
-        val videoStore = VideoStore(listOf(oceansEleven))
-        val movie: Movie? = videoStore.findMovie("Ocean's Eleven")
+        val moviesCollection = listOf(oceansEleven)
+        val videoStore = VideoStore(moviesCollection)
 
-        assertThat(movie, equalTo(oceansEleven))
+        val actual = videoStore.findMovie("Ocean's Eleven")
+
+        assertThat(actual, equalTo(oceansEleven))
     }
 
     @Test fun `find all movies came out in a specific year`() {
-        val oceansEleven = Movie("Ocean's Eleven", 2001, "no", listOf("George Clooney", "Brad Pitt", "Julia Roberts"))
-        val harryPotter = Movie("Harry Potter and the Sorcerer's Stone", 2001, "no", listOf("Daniel Radcliffe", " Rupert Grint", "Richard Harris"))
-        val donnieDarko = Movie("Donnie Darko", 2001, "no", listOf("Jake Gyllenhaal", "Jena Malone", "Mary McDonnell"))
-        val movieCollection = listOf(oceansEleven, harryPotter, donnieDarko)
-        val videoStore = VideoStore(movieCollection)
-        val movies = videoStore.cameOutIn(2001)
+        val moviesCollection = listOf(oceansEleven, harryPotter, donnieDarko)
+        val videoStore = VideoStore(moviesCollection)
 
-        assertThat(movies, equalTo(movieCollection))
+        val actual = videoStore.cameOutIn(2001)
+
+        assertThat(actual, equalTo(moviesCollection))
     }
 
     @Test fun `no movies found in a specific year`() {
-        val oceansEleven = Movie("Ocean's Eleven", 2001, "no", listOf("George Clooney", "Brad Pitt", "Julia Roberts"))
-        val harryPotter = Movie("Harry Potter and the Sorcerer's Stone", 2001, "no", listOf("Daniel Radcliffe", " Rupert Grint", "Richard Harris"))
-        val donnieDarko = Movie("Donnie Darko", 2001, "no", listOf("Jake Gyllenhaal", "Jena Malone", "Mary McDonnell"))
-        val movieCollection = listOf(oceansEleven, harryPotter, donnieDarko)
-        val videoStore = VideoStore(movieCollection)
-        val movies = videoStore.cameOutIn(2002)
+        val moviesCollection = listOf(oceansEleven, harryPotter, donnieDarko)
+        val videoStore = VideoStore(moviesCollection)
 
-        assertThat(movies, equalTo(emptyList()))
+        val actual = videoStore.cameOutIn(2002)
+
+        assertThat(actual, equalTo(emptyList()))
     }
 
     @Test fun `award winner in a specific year`() {
-        val theoryOfEverything = Movie("The Theory of Everything", 2015, "yes", listOf("Eddie Redmayne", "Felicity Jones", "Tom Prior"))
-        val birdman = Movie("Birdman or (The Unexpected Virtue of Ignorance)", 2015, "yes", listOf("Michael Keaton", "Zach Galifianakis", "Edward Norton"))
-        val donnieDarko = Movie("Donnie Darko", 2001, "no", listOf("Jake Gyllenhaal", "Jena Malone", "Mary McDonnell"))
-        val movieCollection = listOf(theoryOfEverything, birdman, donnieDarko)
-        val videoStore = VideoStore(movieCollection)
-        val movies = videoStore.awardWinner(true, 2015)
+        val moviesCollection = listOf(theoryOfEverything, birdman, donnieDarko)
 
-        assertThat(movies, equalTo(listOf(theoryOfEverything, birdman)))
+        val videoStore = VideoStore(moviesCollection)
+        val actual = videoStore.awardWinner(true, 2015)
+
+        assertThat(actual, equalTo(listOf(theoryOfEverything, birdman)))
     }
 
     @Test fun `actors performing in a movie in a specific year`() {
-        val nineteenhundred = Movie("1900", 1976, "no", listOf("Robert De Niro", "Gérard Depardieu"))
-        val allPresidentsMan = Movie("All the President's men", 1976, "no", listOf("Dustin Hoffman", "Robert Redford"))
-        val movieCollection = listOf(nineteenhundred, allPresidentsMan)
+        val moviesCollection = listOf(nineteenhundred, allPresidentsMan)
+        val videoStore = VideoStore(moviesCollection)
 
-        val videoStore = VideoStore(movieCollection)
-        val movies = videoStore.moviesWithSameActorName("Robert")
+        val actual = videoStore.moviesWithSameActorName("Robert")
 
-        assertThat(movies, equalTo(listOf(nineteenhundred, allPresidentsMan)))
+        assertThat(actual, equalTo(listOf(nineteenhundred, allPresidentsMan)))
     }
 
 }
---
+++ src/test/kotlin/sn/movies-fixtures.kt
@@ -0,0 +1,9 @@
+package sn
+
+val oceansEleven = Movie("Ocean's Eleven", 2001, "no", listOf("George Clooney", "Brad Pitt", "Julia Roberts"))
+val harryPotter = Movie("Harry Potter and the Sorcerer's Stone", 2001, "no", listOf("Daniel Radcliffe", " Rupert Grint", "Richard Harris"))
+val donnieDarko = Movie("Donnie Darko", 2001, "no", listOf("Jake Gyllenhaal", "Jena Malone", "Mary McDonnell"))
+val theoryOfEverything = Movie("The Theory of Everything", 2015, "yes", listOf("Eddie Redmayne", "Felicity Jones", "Tom Prior"))
+val birdman = Movie("Birdman or (The Unexpected Virtue of Ignorance)", 2015, "yes", listOf("Michael Keaton", "Zach Galifianakis", "Edward Norton"))
+val nineteenhundred = Movie("1900", 1976, "no", listOf("Robert De Niro", "Gérard Depardieu"))
+val allPresidentsMan = Movie("All the President's men", 1976, "no", listOf("Dustin Hoffman", "Robert Redford"))
