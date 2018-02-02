236c9068551960c2b7fa33ce6a019e8a04700d9f
Diego <diego.gliosca@gmail.com>
2018-02-02 11:40:15 +0000

    formatting

--- src/main/kotlin/sn/VideoStore.kt
+++ src/main/kotlin/sn/VideoStore.kt
@@ -3,14 +3,10 @@ package sn
 class VideoStore(private val movies: List<Movie>) {
 
     fun findMovie(title: String) = movies.find { it.title == title }
-
     fun cameOutIn(year: Int) = movies.filter { it.year == year }
-
     fun awardWinner(awardWinner: Boolean, year: Int) = movies.filter { isAwardWinner(it) == awardWinner }.filter { it.year == year }
-
     fun moviesWithSameActorName(actorName: String) = movies.filter { it.actors.containsActor(actorName) }
 
     private fun isAwardWinner(it: Movie) = it.awardWinner == "yes"
-
     private fun List<String>.containsActor(actorName: String) = this.find { it.contains(actorName) } != null
 }
