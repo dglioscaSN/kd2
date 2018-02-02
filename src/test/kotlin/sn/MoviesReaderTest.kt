5d799572f93f4d2b69567300ec6b0852365d3ce1
Diego <diego.gliosca@gmail.com>
2018-02-02 11:42:26 +0000

    removed redundant tests

--- src/test/kotlin/sn/MoviesReaderTest.kt
+++ src/test/kotlin/sn/MoviesReaderTest.kt
@@ -5,34 +5,24 @@ import org.hamcrest.Matchers.equalTo
 import org.junit.Test
 
 class MoviesReaderTest {
+    private val movies = "movies.txt"
 
     @Test fun `read movies from file`() {
-        val actual = MoviesReader.readMoviesFrom("movies.txt")
+        val actual = MoviesReader.readMoviesFrom(movies)
 
         assertThat(actual.size, equalTo(5))
     }
 
     @Test fun `movies title from file tab separated`() {
-        val actual = MoviesReader.readMoviesFrom("movies.txt")
+        val actual = MoviesReader.readMoviesFrom(movies)
 
         assertThat(actual[0].title, equalTo("Taxi driver"))
     }
 
     @Test fun `read all movies info from tab separated file`() {
-        val actual = MoviesReader.readMoviesFrom("movies.txt")
+        val actual = MoviesReader.readMoviesFrom(movies)
 
         assertThat(actual[0], equalTo(Movie("Taxi driver", 1976, "yes", listOf("Robert De Niro", "Jodie Foster"))))
     }
 
-    @Test fun `make year a number`() {
-        val actual = MoviesReader.readMoviesFrom("movies.txt")
-
-        assertThat(actual[0], equalTo(Movie("Taxi driver", 1976, "yes", listOf("Robert De Niro", "Jodie Foster"))))
-    }
-
-    @Test fun `split actors into a list`() {
-        val actual = MoviesReader.readMoviesFrom("movies.txt")
-
-        assertThat(actual[0], equalTo(Movie("Taxi driver", 1976, "yes", listOf("Robert De Niro", "Jodie Foster"))))
-    }
 }
